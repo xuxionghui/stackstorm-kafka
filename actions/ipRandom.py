efb9370cd44b011b8612bed4337a22b04c79f1a8
xuxionghui <40048549+xuxionghui@users.noreply.github.com>
2021-05-28 15:23:37 +0800

    Update ipRandom.py

--- actions/ipRandom.py
+++ actions/ipRandom.py
@@ -5,4 +5,4 @@ from st2common.runners.base_action import Action
 
 class IpRandomAction(Action):
     def run(self):
-        print(socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff))))
+        return(str(socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))))
